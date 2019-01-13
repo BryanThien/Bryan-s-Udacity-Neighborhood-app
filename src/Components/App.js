b1318009ce7f56ca82c68b57cceeb01abca26e09
Bryan Thien <eonearthproductions@gmail.com>
2019-01-13 17:46:00 -0600

    Adds 5 restaurant markers

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import '../App.css';
 import MapContainer from './mapContainer.js'
-
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 class App extends Component {
+  state = {
+    lat: 44.85994 ,
+    lon: -93.53345,
+    zoom: 16,
+}
   render() {
     return (
     <div className="App">
     <div>
       <h1>Chanhassen MN Stores</h1>
     </div>
-      <MapContainer/>
+      <MapContainer
+      lat={this.state.lat}
+      lon={this.state.lon}
+      zoom={this.state.zoom}
+      />
     </div>
       
     );
--- src/Components/mapContainer.js
+++ src/Components/mapContainer.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 const map_key = 'AIzaSyCKnu3YbAv8wC4wOoZZvx8n4zSrvpxggjk';
 
 class MapContainer extends Component {
-  render() {
-      const style = {
+    render() {
+        const center = {
+            lat: this.props.lat,
+            lng: this.props.lon
+        }
+        const style = {
           width: '100vw',
           height: '100vh'
       }
     return (
     <div style={style}>
-        <Map google={this.props.google} />
+        <Map 
+        google={this.props.google} 
+        initialCenter={center}
+        zoom={this.props.zoom}>
+            <Marker
+          title={"Axel's"}
+          name={"Axel's Restaurant"}
+          position={{lat: 44.8624716, lng: -93.5347458}}
+            />
+            <Marker
+          title={"Kai's"}
+          name={"Kai's Sushi and Grill"}
+          position={{lat: 44.8627453,lng: -93.5355888}}
+            />
+            <Marker
+          title={"Wild Wings"}
+          name={"Buffalo Wild Wings"}
+          position={{lat: 44.8594136,lng: -93.5343053}}
+            />
+            <Marker
+          title={"Smashburger"}
+          name={"Smashburger"}
+          position={{lat: 44.8598655,lng: -93.53028499999999}}
+            />
+            <Marker
+          title={"Davanni's"}
+          name={"Davanni's Pizza & Hot Hoagies"}
+          position={{lat: 44.8567535,lng: -93.53273809999999}}
+            />
+        </Map>
     </div>
     );
   }
